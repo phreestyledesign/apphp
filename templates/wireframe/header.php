ebf8eaa442de97e2788930c5be8cb4d3ecf25ed1
phreestyledesign <rob@phreestyledesign.com>
2013-09-19 13:50:57 +0800

    Added new apphp install

---
+++ templates/wireframe/header.php
@@ -0,0 +1,15 @@
+<?php defined("APPHP_EXEC") or die("Restricted Access"); ?>
+<h1>
+	<a href="<?php echo APPHP_BASE; ?>index.php">
+		<?php echo ($objLogin->IsLoggedInAsAdmin() && $preview != "yes") ? _ADMIN_PANEL : $objSiteDescription->DrawHeader("header_text"); ?>
+	</a>
+</h1>
+
+<?php
+	if($objLogin->IsLoggedInAsAdmin() && $preview == "yes"){
+		echo prepare_permanent_link('index.php?preview=no', _BACK_TO_ADMIN_PANEL, "", "header");
+	}else{
+		//echo $objSiteDescription->GetParameter("slogan_text");				
+	}
+?>
+
